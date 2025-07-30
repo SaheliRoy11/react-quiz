688b28acb38e6b646d14dc1b66930b612596c633
Saheli <engr.saheliroy@gmail.com>
2025-07-30 21:14:01 +0530

    Create Main component, simulate RestfulAPI using questions.json as data source and create basic structure for our App using the useReducer hook for state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
-export default function App () {
-  return <div>
-    <DateCounter />
-  </div>
+const initialState = {
+  questions: [],
+  //to tell the user the status of our app at the moment, in the beginning the app will be in the loading state. The app can be in any of the following statuses, those are 'loading', 'error', 'ready' (once data has arrived), 'active' (quiz is running), 'finished' (quiz completed).
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //fetch data from API
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main></Main>
+    </div>
+  );
 }
