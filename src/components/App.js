96f027e8f80d5ef5255f9f32461ba948fb05bc80
Saheli <engr.saheliroy@gmail.com>
2025-08-01 19:44:53 +0530

    Refactor file structure to store component files in separate folder inside src

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+  //to tell the user the status of our app at the moment, in the beginning the app will be in the loading state. The app can be in any of the following statuses, those are 'loading', 'error', 'ready' (once data has arrived), 'active' (quiz is running), 'finished' (quiz completed).
+  status: "loading",
+  index: 0 //keeps track of currently displayed question
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status, index } = state;
+  const numQuestions = questions.length;
+
+  //fetch data from API
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question = {questions[index]}/>}
+      </Main>
+    </div>
+  );
+}
